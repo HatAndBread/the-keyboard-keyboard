8f0b790b0ba39ad779bb6b5173c85381fad06917
Joshua Hume <valencelectron@hotmail.com>
2021-05-29 08:52:13 +0900

    edit key modal

---
+++ src/app/components/modal/EditKeyModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditKeyModal = ({ myKey }: { myKey: string | null }) => {
+  return <div>{myKey}</div>;
+};
+
+export default EditKeyModal;
--- src/app/components/modal/ModalController.tsx
+++ src/app/components/modal/ModalController.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Modal from "./Modal";
-import { useAppSelector } from "../../hooks";
-import { keyBeingEdited } from "../../../features/startedSlice";
+import React from 'react';
+import Modal from './Modal';
+import { useAppSelector } from '../../hooks';
+import { keyBeingEdited } from '../../../features/startedSlice';
+import EditKeyModal from './EditKeyModal';
 
 const ModalController = ({ currentModal }: { currentModal: string | null }) => {
   const key = useAppSelector(keyBeingEdited);
   const getModalContent = () => {
     switch (currentModal) {
-      case "edit-key":
-        return <div>WOW</div>;
+      case 'edit-key':
+        return <EditKeyModal myKey={key} />;
       default:
         return <></>;
     }

6fb0e22c7921ae3c4d65ce4c1b2c013a86bcdc8a
Joshua Hume <valencelectron@hotmail.com>
2021-06-01 22:50:51 +0900

    is valid key

--- src/app/music-logic/default-keyboards/keyboard-template.ts
+++ src/app/music-logic/default-keyboards/keyboard-template.ts
@@ -513,8 +513,50 @@ const keyboardTemplate: KeyboardTemplate = {
 
 const getKeyboardTemplate = () => _.cloneDeep(keyboardTemplate);
 
-const isValidKey = (key: string) => {
-  if (key === 'a' || key === 'b') {
+export const isValidKey = (k: string) => {
+  if (
+    k === 'a' ||
+    k === 'b' ||
+    k === 'c' ||
+    k === 'd' ||
+    k === 'e' ||
+    k === 'f' ||
+    k === 'g' ||
+    k === 'h' ||
+    k === 'i' ||
+    k === 'j' ||
+    k === 'k' ||
+    k === 'l' ||
+    k === 'm' ||
+    k === 'n' ||
+    k === 'o' ||
+    k === 'p' ||
+    k === 'q' ||
+    k === 'r' ||
+    k === 's' ||
+    k === 't' ||
+    k === 'u' ||
+    k === 'v' ||
+    k === 'w' ||
+    k === 'x' ||
+    k === 'y' ||
+    k === 'z' ||
+    k === '1' ||
+    k === '2' ||
+    k === '3' ||
+    k === '4' ||
+    k === '5' ||
+    k === '6' ||
+    k === '7' ||
+    k === '8' ||
+    k === '9' ||
+    k === '0' ||
+    k === ' ' ||
+    k === ',' ||
+    k === '.' ||
+    k === '?' ||
+    k === ';'
+  ) {
     return true;
   }
 };

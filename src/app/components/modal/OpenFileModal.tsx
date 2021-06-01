2da8a3b038f2f2d109472afac6dbd5a3c0803938
Joshua Hume <valencelectron@hotmail.com>
2021-06-01 17:33:42 +0900

    opener getting files2

--- src/app/components/modal/OpenFileModal.tsx
+++ src/app/components/modal/OpenFileModal.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 const OpenFileModal = () => {
+  const open = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const fileToLoad = e.target.files[0];
+      const fileReader = new FileReader();
+      fileReader.onload = function (
+        fileLoadedEvent: ProgressEvent<FileReader>
+      ) {
+        if (fileLoadedEvent.target?.result) {
+          const textFromFileLoaded = fileLoadedEvent.target.result;
+          if (typeof textFromFileLoaded === 'string') {
+            const result = JSON.parse(textFromFileLoaded);
+            if (!result) {
+              alert('Not a valid file. Please try again. ✨');
+            } else {
+              console.log(result);
+            }
+          }
+        }
+      };
+
+      fileReader.readAsText(fileToLoad, 'UTF-8');
+    }
+  };
   return (
     <div className='OpenFileModal'>
-      <input type='file' name='file-open' id='file-open' />
+      <input
+        type='file'
+        name='file-open'
+        id='file-open'
+        accept='.s2pd'
+        onChange={open}
+      />
+      <div className='open-modal-buttons'>
+        <button>Open</button>
+        <button>Cancel</button>
+      </div>
     </div>
   );
 };
